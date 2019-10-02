71f791e3c48d351e63248fb86db761d99d483f92
CiGit-Franck <franck.thery@gmail.com>
2019-10-02 16:33:42 +0200

    na

--- conditions.py
+++ conditions.py
@@ -1,13 +1,15 @@
 # Etape 3
 periode = "a definir"
-heure = 15
+heure = 19
 
 if heure < 0 or heure > 24 :
     print("Erreur de saisie")
 else:
     if heure < 10 :
         periode = "matinée"
-    if heure < 18 :
+    elif heure < 18 :
         periode = "après midi"
+    else:
+        periode = "soirée"
     
     print("bonne "+periode)
