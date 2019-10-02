63b92f367cc0fe49366565e0bb339227ac7927a2
CiGit-Franck <franck.thery@gmail.com>
2019-10-02 15:43:01 +0200

    first

---
+++ oprateurs.py
@@ -0,0 +1,42 @@
+# exo 1
+mystery1 = 8 + 6
+mystery2 = 8 - 6
+
+print("Le nombre mystère est : "+str(mystery2))
+
+# exo 2
+zebrasInZoo = 8
+giraffesInZoo = 4
+
+animalsInZoo = zebrasInZoo + giraffesInZoo
+
+print("Le zoo contient : "+str(animalsInZoo)+" animaux")
+
+# exo 3
+prixInitial = 1500
+tva = 0.20
+
+prixFinal = prixInitial * (1+tva)
+
+print("Le prix final est : "+str(prixFinal))
+
+# exo 4 - 1
+nbOeufsParBoite = 6
+nbOeufsTotal = 145
+
+nbBoites = nbOeufsTotal // nbOeufsParBoite
+nbOeufsRestant = nbOeufsTotal % nbOeufsParBoite
+
+print("Nous avons "+str(nbBoites)+" boites de 6 oeufs est il reste "+str(nbOeufsRestant)+" oeufs")
+
+# exo 4 - 2
+# figures (7,8,9,.., roi, as) vaut (0,1,2 ... 6, 7)
+# couleurs (pique, cÅ“ur, carreau, trÃ¨fle)) vaut (0,1,2 ou 3)
+# Exemple :
+# le 9 de pique a pour numÃ©ro = 2+8*0 = 2 (3e carte de la couleur 0)
+# la dame de cÅ“ur a pour numÃ©ro = 5+8*1 = 13
+
+numeroCarteMystere = 28 # numeroFigure * numeroCouleur
+# enoncé pas bon :-/
+print("Pschittttt pour la carte ...")
+
