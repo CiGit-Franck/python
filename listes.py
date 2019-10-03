e88e6f6a504fe750ab0a73dd526fbec00d62d330
CiGit-Franck <franck.thery@gmail.com>
2019-10-03 10:13:12 +0200

    list ok

---
+++ listes.py
@@ -0,0 +1,11 @@
+promo = ['benoit', 'franck L','philippe', 'guillaume', 'fred','carole','nicolas','pierre','saya','vincent','sylvain','jean marc', 'franck T', 'yben?']
+
+promo.sort()
+print(promo)
+
+indice_impair = list(promo)
+
+print(indice_impair)
+
+groupe_1 = indice_impair
+# groupe_2 = 
