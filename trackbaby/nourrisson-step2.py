a4b84b0949c670134d0c61ba5538a4de2fad3e40
CiGit-Franck <franck.thery@gmail.com>
2019-10-04 17:46:22 +0200

    step2 : graph ko

--- trackbaby/nourrisson-step2.py
+++ trackbaby/nourrisson-step2.py
@@ -31,29 +31,34 @@ tailles_gars  = matrice_csv('./taille-age-garcon-0-60-light.csv')
 cranes_fille = matrice_csv('./perim-cra-age-fille-0-60-light.csv')
 cranes_gars  = matrice_csv('./perim-cra-age-garcon-0-60-light.csv')
 
-# print(poids_gars[1]) # debug
+# print(format_list(poids_gars[0])) # debug
 
 # Affichage des graphiques à faire
 x = format_list(poids_gars[0])
+# matrice de poids 
+poids = plt.subplot(1, 3, 1)
+poids.plot(x, format_list(poids_gars[1]), c='#448abd', linestyle = 'solid', label='5%') # p5
+poids.plot(x, format_list(poids_gars[2]), c='#f29739', linestyle = 'solid', label='25%') # p25
+poids.plot(x, format_list(poids_gars[3]), c='#59a94a', linestyle = 'solid', label='50%') # p50
+poids.plot(x, format_list(poids_gars[4]), c='#cf4b3e', linestyle = 'solid', label='75%') # p75
+poids.plot(x, format_list(poids_gars[5]), c='#9f82c4', linestyle = 'solid', label='95%') # p95
+poids.scatter(format_list(mesures[0]), format_list(mesures[1]), c='black') # mesures
+plt.ylabel('Poids en kg')
+plt.xlabel('Age en mois')
+plt.legend()
+plt.grid()
+# matrice de taille
+taille = plt.subplot(1, 3, 2)
+taille.scatter(format_list(mesures[0]), format_list(mesures[2]), c='black') # mesures
+plt.ylabel('Taille en cm')
 plt.xlabel('Age en mois')
 plt.grid()
-# matrice de poids 
-fig = plt.subplot(1, 3, 1)
-fig.plot(x, format_list(poids_gars[1]), c='blue', linestyle = 'solid') # p5
-fig.plot(x, format_list(poids_gars[2]), c='orange', linestyle = 'solid') # p25
-fig.plot(x, format_list(poids_gars[3]), c='green', linestyle = 'solid') # p50
-fig.plot(x, format_list(poids_gars[4]), c='red', linestyle = 'solid') # p75
-fig.plot(x, format_list(poids_gars[5]), c='purple', linestyle = 'solid') # p95
-fig.scatter(format_list(mesures[0]), format_list(mesures[1]), c='black') # mesures
-plt.ylabel('Poids en kg')
-# matrice de taille
-fig = plt.subplot(1, 3, 2)
-fig.scatter(format_list(mesures[0]), format_list(mesures[2]), c='black') # mesures
-plt.ylabel('Taille en cm')
 # matrice du perimetre cranien
-fig = plt.subplot(1, 3, 3)
-fig.scatter(format_list(mesures[0]), format_list(mesures[3]), c='black') # mesures
+crane = plt.subplot(1, 3, 3)
+crane.scatter(format_list(mesures[0]), format_list(mesures[3]), c='black') # mesures
 plt.ylabel('Perimetre cranien en cm')
+plt.xlabel('Age en mois')
+plt.grid()
 
 plt.show()
 
